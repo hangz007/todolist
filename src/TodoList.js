6231556d5d68b2c1056e93972ee63e72bc052f86
hangz007 <hangz00749@163.com>
2020-01-04 09:35:19 +0800

    输入框添加构造函数

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,17 @@
 import React,{Component,Fragment} from 'react';
 
 class TodoList extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputVaule:'',
+            list:[]
+        }
+    }
     render() {
         return (
             <Fragment>
-                <div><input /><button>提交</button></div>
+                <div><input value={this.state.inputVaule} /><button>提交</button></div>
                 <ul>
                     <li>学英语</li>
                     <li>Learning React</li>
