f2ace579318d1529cbe1f278effce3753a396f6f
hangz007 <hangz00749@163.com>
2020-01-07 14:05:25 +0800

    TodoList细节优化

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-    hello world !
-   </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+       hello world !
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,5 @@
 import React,{Component,Fragment} from 'react';
+import './style.css'
 
 class TodoList extends Component{
     constructor(props) {
@@ -11,12 +12,17 @@ class TodoList extends Component{
     render() {
         return (
             <Fragment>
-                <div><input value={this.state.inputValue} onChange={this.handleInputChange.bind(this)} />
+                <div>
+                    <label htmlFor="insertArea" >输入内容</label>
+                    <input id="insertArea" className="input" value={this.state.inputValue} onChange={this.handleInputChange.bind(this)} />
                 <button onClick={this.handleBtnClick.bind(this)} >提交</button></div>
                 <ul>
                     {
                         this.state.list.map(
-                            (item,index)=> {return <li key={index} onClick={this.handleItemDelete.bind(this,index)} >{item}</li>}
+                            (item,index)=> {
+                                return <li key={index} onClick={this.handleItemDelete.bind(this,index)} 
+                                dangerouslySetInnerHTML={{__html:item}} >
+                                </li>}
                         )
                      }
                 </ul>
@@ -27,12 +33,14 @@ class TodoList extends Component{
     handleInputChange(e) {
         this.setState({inputValue: e.target.value});
     }
+
     handleBtnClick() {
         this.setState({
             list:[...this.state.list,this.state.inputValue],
             inputValue:''
         })
     }
+
     handleItemDelete(index) {
         const list = [...this.state.list];
         list.splice(index,1);
@@ -40,6 +48,7 @@ class TodoList extends Component{
             list:list
         });
     }
+    
 }
 
 export default TodoList;
