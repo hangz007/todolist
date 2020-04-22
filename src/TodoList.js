c123c30f2af2ae3f6be320a8144d49a643fe4d3f
hangz007 <hangz00749@163.com>
2020-04-22 10:04:46 +0800

    使用React-redux重构TodoList

---
+++ src/TodoList.js
@@ -0,0 +1,60 @@
+import React,{ Component } from 'react';
+import { connect } from 'react-redux';
+
+class TodoList extends Component {
+
+    render() {
+        const {inputValue,list,changeInputValue,handleClick,handleDelete} = this.props;
+        return (
+        <div>
+            <div>
+                <input value={inputValue} onChange={changeInputValue.bind(this)} />
+                <button onClick={handleClick.bind(this)}>提交</button>
+            </div>
+            <ul>
+                {
+                    list.map((item,index) =>{
+                        return (<li key={index} onClick={()=> {handleDelete(index)}} >{item}</li>)
+                    })
+                }
+            </ul>
+        </div>
+        );
+
+    }
+
+}
+
+const mapStateToProps = (state) => {
+    return {
+        inputValue:state.inputValue,
+        list:state.list
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        changeInputValue(e) {
+            const action = {
+                type: 'change_input_value',
+                value: e.target.value
+            };
+            dispatch(action);
+        },
+        handleClick() {
+            const action = {
+                type:  'add_item'
+            };
+            dispatch(action);
+        },
+        handleDelete(index) {
+            const action = {
+                type:'delete_item',
+                index
+            }
+            dispatch(action);
+        }
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(TodoList);
